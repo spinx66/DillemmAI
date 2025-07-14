7df3a6431cb3eee5bd10a8df0bf8822a47747b39
Spinx <132048373+spinx66@users.noreply.github.com>
2025-07-14 20:15:19 +0530

    Update state.py
    Removed def

--- core/state.py
+++ core/state.py
@@ -6,15 +6,3 @@ def init_session_state():
     for key in ["questions", "answers", "options"]:
         if key not in st.session_state:
             st.session_state[key] = []
-
-def handle_tag_removal():
-    query = st.query_params
-    if "remove_tag" in query:
-        try:
-            index = int(query["remove_tag"])
-            if 0 <= index < len(st.session_state.options):
-                st.session_state.options.pop(index)
-                st.query_params.clear()
-                st.rerun()
-        except:
-            pass
