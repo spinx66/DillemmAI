4d203108463079614a2e1aba6b8bad6bf943b689
Spinx <132048373+spinx66@users.noreply.github.com>
2025-07-14 20:18:58 +0530

    Update state.py
    Added reset function

--- core/state.py
+++ core/state.py
@@ -6,3 +6,8 @@ def init_session_state():
     for key in ["questions", "answers", "options"]:
         if key not in st.session_state:
             st.session_state[key] = []
+
+def reset_all():
+    st.session_state.questions = []
+    st.session_state.answers = {}
+    st.session_state.options = []
