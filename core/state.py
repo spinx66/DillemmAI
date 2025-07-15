e038d185a9fe98b636dc37543f9405786aa8800a
Spinx <132048373+spinx66@users.noreply.github.com>
2025-07-15 21:02:43 +0530

    Update state.py
    r

--- core/state.py
+++ core/state.py
@@ -3,7 +3,7 @@
 import streamlit as st
 
 def init_session_state():
-    for key in ["questions", "answers", "options"]:
+    for key in ["questions", "answers", "options", "result"]:
         if key not in st.session_state:
             st.session_state[key] = []
 
@@ -12,5 +12,3 @@ def reset_all():
     st.session_state.answers = {}
     st.session_state.options = []
     st.session_state.result = None
-    st.session_state.main_purpose = ""
-    st.session_state.reset_flag = True
